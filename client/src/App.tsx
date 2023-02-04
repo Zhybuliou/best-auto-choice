c274fcaef99e6413d44c2f9b3ae5e2661ae718c9
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2023-02-04 12:15:30 +0300

    feat: add matirial ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import './App.css'
 
-
 const App = () => {
-  return (
-    <div className='App'>
-    </div>
-  )
+  return <div className='App'></div>
 }
 
 export default App
--- client/src/Cars.tsx
+++ client/src/Cars.tsx
@@ -6,16 +6,19 @@ export default function Cars() {
     <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
       {dataCars.adminEvents.media.medias.map((img) => {
         const objCar = dataCars.adminEvents.assets.assets
-        const {vin, model, year, make, mileage, exteriorColor, fuelType} = objCar[img.entityId as unknown as keyof typeof objCar].searchable
+        const { vin, model, year, make, mileage, exteriorColor, fuelType } =
+          objCar[img.entityId as unknown as keyof typeof objCar].searchable
         return (
           <div key={img.id} style={{ display: 'flex', margin: '5px' }}>
             <img src={img.urls.medium} style={{ width: '250px' }} />
             <div style={{ width: '150px', marginLeft: '10px' }}>
-            <h3>{make} {model} {year}</h3>
-            <h5>mile: {mileage}</h5>
-            <h5>vin: {vin}</h5>
-            <h5>exteriorColor: {exteriorColor}</h5>
-            <h5>fuelType: {fuelType}</h5>
+              <h3>
+                {make} {model} {year}
+              </h3>
+              <h5>mile: {mileage}</h5>
+              <h5>vin: {vin}</h5>
+              <h5>exteriorColor: {exteriorColor}</h5>
+              <h5>fuelType: {fuelType}</h5>
             </div>
           </div>
         )
